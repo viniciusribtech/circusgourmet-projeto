d72c374dd0a88a9a4937076080d78a75607c5f80
paulovdsoares <pvdsoares@gmail.com>
2026-09-16 15:42:00 -0300

    criação componentes CabecalhoPagina, Botao e OcupacaoCarrinhos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Home from './pages/home.jsx';
+import Home from './pages/Home/home';
 
 
 function App() {
---
+++ frontend/src/pages/Home/home.jsx
@@ -0,0 +1,37 @@
+import Navbar from "../../components/BarraNav/navbar.jsx";
+import Calendar from "../../components/Calendario/calendar.jsx";
+import CabecalhoPagina from "../../components/CabecalhoPagina/cabecalhoPagina.jsx";
+import Botao from "../../components/Botao/botao.jsx";
+import OcupacaoCarrinhos from "../../components/OcupacaoCarrinhos/ocupacaoCarrinhos.jsx";
+
+import "./home.css";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="conteudo-principal">
+        <div className="linha-titulo">
+          <CabecalhoPagina
+            titulo="Calendário Operacional"
+            subtitulo="Gestão centralizada de eventos e logística de carrinhos gourmet."
+          />
+          <Botao onClick={() => alert("Abrir modal de novo evento")}>
+            Novo Evento
+          </Botao>
+        </div>
+
+        <div className="grid-principal">
+          <div className="coluna-calendario">
+            <Calendar />
+          </div>
+          <div className="coluna-lateral">
+            <OcupacaoCarrinhos carrinhos={[]} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
